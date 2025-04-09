dfb44241223384b18e72abe142da69f5c32a3cd0
k-kahora <mango22mjk@gmail.com>
2025-04-09 12:32:53 -0400

    auth added

--- lib/jobsync_web/live/landing_live/landing.ex
+++ lib/jobsync_web/live/landing_live/landing.ex
@@ -1,13 +1,25 @@
 defmodule JobsyncWeb.Landing do
   use JobsyncWeb, :live_view
   alias JobsyncWeb.Landing.Component
+  alias Jobsync.Accounts
 
   def mount(_params, session, socket) do
-    {:ok, assign(socket, title: "JobSync", score: 100_000_000)}
+    user = Accounts.get_user_by_session_token(session["user_token"])
+
+    {:ok,
+     assign(socket,
+       title: "JobSync",
+       score: 100_000_000
+       # session_id: session["live_socket_id"]
+     )}
   end
 
   def render(assigns) do
     ~H"""
+    <pre>
+      <%= inspect(@current_user.email) %>
+      <%= @session_id %>
+    </pre>
     <div class="flex flex-col items-center lg:items-start gap-10 v-screen">
       <Component.header></Component.header>
       <ul>
--- lib/jobsync_web/router.ex
+++ lib/jobsync_web/router.ex
@@ -1,6 +1,8 @@
 defmodule JobsyncWeb.Router do
   use JobsyncWeb, :router
 
+  import JobsyncWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule JobsyncWeb.Router do
     plug :put_root_layout, html: {JobsyncWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -21,7 +24,6 @@ defmodule JobsyncWeb.Router do
     live "/jobs", JobLive.Index, :index
     live "/jobs/new", JobLive.Index, :new
     live "/jobs/:id/edit", JobLive.Index, :edit
-    live "/landing", Landing
 
     live "/jobs/:id", JobLive.Show, :show
     live "/jobs/:id/show/edit", JobLive.Show, :edit
@@ -46,6 +48,46 @@ defmodule JobsyncWeb.Router do
 
       live_dashboard "/dashboard", metrics: JobsyncWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", JobsyncWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{JobsyncWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", JobsyncWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{JobsyncWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+
+      live "/landing", Landing
+    end
+  end
+
+  scope "/", JobsyncWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{JobsyncWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
