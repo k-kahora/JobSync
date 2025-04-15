6706f348209717bc1a3b215e03ea8d400d6bb40c
k-kahora <mango22mjk@gmail.com>
2025-04-14 21:33:37 -0400

    migrations

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,33 +9,3 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
-alias Jobsync.Positions
-
-jobs = [
-  %{
-    state: "applied",
-    description: "Who cares",
-    title: "software engineer",
-    date_applied: ~D[2022-01-01],
-    company: "amazon",
-    notes: "none"
-  },
-  %{
-    state: "applied",
-    description: "Who cares",
-    title: "software engineer",
-    date_applied: ~D[2022-01-01],
-    company: "walmart",
-    notes: "none"
-  },
-  %{
-    state: "applied",
-    description: "Who cares",
-    title: "software engineer",
-    date_applied: ~D[2022-01-01],
-    company: "apple",
-    notes: "none"
-  }
-]
-
-Enum.each(jobs, fn job -> Positions.create_job(job) end)
