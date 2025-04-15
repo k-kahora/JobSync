a86256a768279550b157f967fa6df8ad6d1544db
k-kahora <mango22mjk@gmail.com>
2025-04-15 14:27:06 -0400

    pre migration fault

--- lib/jobsync_web/live/survey_live/job_list.ex
+++ lib/jobsync_web/live/survey_live/job_list.ex
@@ -2,4 +2,13 @@ defmodule JobsyncWeb.SurveyLive.JobList do
   use JobsyncWeb, :live_component
   alias Jobsync.Applications
   alias Jobsync.Applications.Jobs
+
+  def update(assigns, socket) do
+    {:ok, socket |> assign(assigns) |> assign(:jobs, "No jobs") |> assign_jobs()}
+  end
+
+  defp assign_jobs(%{assigns: %{current_user: current_user}} = socket) do
+    # assign(socket, :jobs, Applications.get_jobs_by_user(current_user))
+    socket
+  end
 end
